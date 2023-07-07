d245cb2942cbe7a8f9469d565c3bdfaedc5a7c6c
notpatrickadams <32269304+notpatrickadams@users.noreply.github.com>
2023-07-07 01:55:31 -0400

    Made /butterfree more sad with more GIFs

--- src/commands/byebutterfree.ts
+++ src/commands/byebutterfree.ts
@@ -1,13 +1,24 @@
 import { CommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
 
+function chooseRandomGif() {
+    const gifChoices = [
+        "https://media.tenor.com/LKMEOvqv1hMAAAAC/ash-butterfree.gif",
+        "https://media.tenor.com/saqrsevceMYAAAAC/butterfree-ash.gif",
+        "https://media.tenor.com/JFtZhvsR0mQAAAAC/triste.gif",
+        "https://media.tenor.com/_hC2SU91dogAAAAC/oh-no.gif"
+    ];
+    return gifChoices[Math.floor(Math.random() * gifChoices.length)];
+}
+
 export const data = new SlashCommandBuilder()
     .setName("butterfree")
     .setDescription("Say goodbye to Butterfree");
 
 export const execute = async (interaction: CommandInteraction) => {
+    const randomGif = chooseRandomGif();
     const butterfreeEmbed = new EmbedBuilder()
         .setTitle("Bye Bye Butterfree")
-        .setImage("https://media.tenor.com/saqrsevceMYAAAAC/butterfree-ash.gif")
+        .setImage(randomGif)
         .toJSON();
     
     await interaction.reply({ embeds: [butterfreeEmbed] });
