b86482a53205afec61cffe78f9dbcfcaf5e14147
notpatrickadams <32269304+notpatrickadams@users.noreply.github.com>
2023-08-18 15:59:47 -0400

    Added /invite
    /invite sends an ephemeral message with the invite link and necessary permissions

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import { CommandInteraction, EmbedBuilder, SlashCommandBuilder, OAuth2Scopes } from "discord.js";
 import { client } from "../constants";
 
 export const data = new SlashCommandBuilder()
@@ -6,13 +6,19 @@ export const data = new SlashCommandBuilder()
     .setDescription("Want to invite UselessBot to your server?");
 
 export const execute = async (interaction: CommandInteraction) => {
-    const clientId = client.application?.id;
-    const embed = new EmbedBuilder()
-        .setTitle("Invite UselessBot to your server!")
-        .setURL(`https://discord.com/oauth2/authorize?client_id=${ clientId }&scope=bot+applications.commands`)
-        .setThumbnail("http://upload.wikimedia.org/wikipedia/commons/c/cf/Curious_Raccoon.jpg")
-        .setDescription("Click the link to invite UselessBot to your server!")
-        .toJSON();        
+    if (client.application) {
+        const scope = [
+            OAuth2Scopes.Bot,
+            OAuth2Scopes.ApplicationsCommands
+        ].join("+");
+        const clientId: string = client.application.id;
+        const embed = new EmbedBuilder()
+            .setTitle("Invite UselessBot to your server!")
+            .setURL(`https://discord.com/oauth2/authorize?client_id=${ clientId }&scope=${ scope }`)
+            .setThumbnail("https://cdn.discordapp.com/avatars/594333252945444879/cd63ae72b1d529b5db39fcaa72c25f3b?size=512")
+            .setDescription("Click the link to invite UselessBot to your server!")
+            .toJSON();        
     
-    await interaction.reply({ ephemeral: true, embeds: [embed] });
+        await interaction.reply({ ephemeral: true, embeds: [embed] });
+    }
 };
